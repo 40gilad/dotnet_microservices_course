e12623c943280da8ff1679623365a205e06ff2d4
40gilad <40gilad.shenkar@gmail.com>
2025-04-15 18:10:14 +0300

    kickoff. until 1:10:27

---
+++ WebApplication1/Data/IPlatformRepo.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+	public interface IPlatformRepo
+	{
+		bool SaveChanges();
+
+		IEnumerable<Platform> GetAll();
+		Platform GetPlatformById(int id);
+		void CreatePlatform(Platform p);
+	}
+}
---
+++ WebApplication1/Data/PlatformRepo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+	public class PlatformRepo : IPlatformRepo
+	{
+
+		private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void CreatePlatform(Platform p)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Platform> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Platform GetPlatformById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool SaveChanges()
+		{
+			return (_context.SaveChanges() >= 0);
+		}
+	}
+}
