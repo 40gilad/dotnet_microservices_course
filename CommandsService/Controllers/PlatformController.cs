994e6000942b60b3d95b7cb15b02cf857896271b
40gilad <40gilad.shenkar@gmail.com>
2025-04-17 17:07:52 +0300

    recreate CommandsService

---
+++ CommandsService/Controllers/PlatformController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+
+	[Route("api/commands/[controller]")]
+	[ApiController]
+	public class PlatformController: ControllerBase
+	{
+        public PlatformController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> CommandService.Inbound");
+            return Ok("Inbound test from CommandService.PlatformController");
+        }
+
+    }
+}
